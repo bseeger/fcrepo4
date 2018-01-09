adf39a43d7239e36b802e24e1e4fb09fb6c0342c
bbpennel <bbpennel@email.unc.edu>
2018-01-09 17:00:29 -0500

    Revert changes to services related to alternate FedoraBinary implementations and factory

--- fcrepo-http-api/src/test/java/org/fcrepo/http/api/FedoraLdpTest.java
+++ fcrepo-http-api/src/test/java/org/fcrepo/http/api/FedoraLdpTest.java
@@ -61,7 +61,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anySetOf;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
@@ -616,7 +615,7 @@ public class FedoraLdpTest {
             testObj.evaluateRequestPreconditions(mockRequest, mockResponse, testObj.resource(),
                     mockSession, true);
             fail("Expected " + PreconditionException.class.getName() + " to be thrown.");
-        } catch (final PreconditionException e) {
+        } catch (PreconditionException e) {
             // expected
         }
 
@@ -656,7 +655,7 @@ public class FedoraLdpTest {
             testObj.evaluateRequestPreconditions(mockRequest, mockResponse, testObj.resource(),
                     mockSession, true);
             fail("Expected " + PreconditionException.class.getName() + " to be thrown.");
-        } catch (final PreconditionException e) {
+        } catch (PreconditionException e) {
             // expected
         }
 
@@ -856,8 +855,7 @@ public class FedoraLdpTest {
         when(mockBinary.isNew()).thenReturn(true);
 
         when(mockNodeService.exists(mockFedoraSession, "/some/path")).thenReturn(false);
-        when(mockBinaryService.findOrCreate(eq(mockFedoraSession), eq("/some/path"), anyString()))
-                .thenReturn(mockBinary);
+        when(mockBinaryService.findOrCreate(mockFedoraSession, "/some/path")).thenReturn(mockBinary);
 
         final Response actual = testObj.createOrReplaceObjectRdf(TEXT_PLAIN_TYPE,
                 toInputStream("xyz", UTF_8), null, null, nonRDFSourceLink, null);
@@ -990,8 +988,7 @@ public class FedoraLdpTest {
     public void testCreateNewBinary() throws MalformedRdfException, InvalidChecksumException,
            IOException, UnsupportedAlgorithmException, UnsupportedAccessTypeException {
         setResource(Container.class);
-        when(mockBinaryService.findOrCreate(eq(mockFedoraSession), eq("/b"), anyString()))
-                .thenReturn(mockBinary);
+        when(mockBinaryService.findOrCreate(mockFedoraSession, "/b")).thenReturn(mockBinary);
         try (final InputStream content = toInputStream("x", UTF_8)) {
             final Response actual = testObj.createObject(null, APPLICATION_OCTET_STREAM_TYPE, "b", content,
                 nonRDFSourceLink, null);
@@ -1004,8 +1001,7 @@ public class FedoraLdpTest {
     public void testCreateNewBinaryWithInsufficientResources() throws MalformedRdfException,
            InvalidChecksumException, IOException, UnsupportedAlgorithmException, UnsupportedAccessTypeException {
         setResource(Container.class);
-        when(mockBinaryService.findOrCreate(eq(mockFedoraSession), eq("/b"), anyString()))
-                .thenReturn(mockBinary);
+        when(mockBinaryService.findOrCreate(mockFedoraSession, "/b")).thenReturn(mockBinary);
 
 
         try (final InputStream content = toInputStream("x", UTF_8)) {
@@ -1027,8 +1023,7 @@ public class FedoraLdpTest {
            InvalidChecksumException, IOException, UnsupportedAlgorithmException, UnsupportedAccessTypeException {
 
         setResource(Container.class);
-        when(mockBinaryService.findOrCreate(eq(mockFedoraSession), eq("/b"), anyString()))
-                .thenReturn(mockBinary);
+        when(mockBinaryService.findOrCreate(mockFedoraSession, "/b")).thenReturn(mockBinary);
         try (final InputStream content = toInputStream("x", UTF_8)) {
             final MediaType requestContentType = MediaType.valueOf("some/mime-type; with=some; param=s");
             final Response actual = testObj.createObject(null, requestContentType, "b", content, nonRDFSourceLink,
@@ -1043,8 +1038,7 @@ public class FedoraLdpTest {
            InvalidChecksumException, IOException, UnsupportedAlgorithmException, UnsupportedAccessTypeException {
 
         setResource(Container.class);
-        when(mockBinaryService.findOrCreate(eq(mockFedoraSession), eq("/b"), anyString()))
-                .thenReturn(mockBinary);
+        when(mockBinaryService.findOrCreate(mockFedoraSession, "/b")).thenReturn(mockBinary);
         try (final InputStream content = toInputStream("x", UTF_8)) {
             final MediaType requestContentType = MediaType.valueOf("some/mime-type; with=some; param=s");
             final String sha = "07a4d371f3b7b6283a8e1230b7ec6764f8287bf2";
@@ -1080,8 +1074,7 @@ public class FedoraLdpTest {
             InvalidChecksumException, IOException, UnsupportedAlgorithmException, UnsupportedAccessTypeException {
 
         setResource(Container.class);
-        when(mockBinaryService.findOrCreate(eq(mockFedoraSession), eq("/b"), anyString()))
-                .thenReturn(mockBinary);
+        when(mockBinaryService.findOrCreate(mockFedoraSession, "/b")).thenReturn(mockBinary);
         try (final InputStream content = toInputStream("x", UTF_8)) {
             final MediaType requestContentType = MediaType.valueOf("some/mime-type; with=some; param=s");
             final String md5 = "HUXZLQLMuI/KZ5KDcJPcOA==";
@@ -1099,8 +1092,7 @@ public class FedoraLdpTest {
            InvalidChecksumException, IOException, UnsupportedAlgorithmException, UnsupportedAccessTypeException {
 
         setResource(Container.class);
-        when(mockBinaryService.findOrCreate(eq(mockFedoraSession), eq("/b"), anyString()))
-                .thenReturn(mockBinary);
+        when(mockBinaryService.findOrCreate(mockFedoraSession, "/b")).thenReturn(mockBinary);
         try (final InputStream content = toInputStream("x", UTF_8)) {
             final MediaType requestContentType = MediaType.valueOf("some/mime-type; with=some; param=s");
 
